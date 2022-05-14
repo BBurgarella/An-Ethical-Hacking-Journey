ef4d28d831dc414bd1c2be426b7670a33bfb2635
BBurgarella <borisbarreur109038@hotmail.fr>
2022-05-14 23:03:36 +0200

    added the header and "licence"

--- transversalExplorer/transversalExplorer.py
+++ transversalExplorer/transversalExplorer.py
@@ -1,3 +1,13 @@
+#!/bin/env python3
+"""
+ * ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * Boris Burgarella wrote this file. As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me a beer in return Poul-Henning Kamp
+ * ----------------------------------------------------------------------------
+"""
+
 import argparse
 import os
 import sys
